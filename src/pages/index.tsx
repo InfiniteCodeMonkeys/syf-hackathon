e87c41966cd28b7173a4ecc996820f078f96aa40
Mike Dyer <mike@chindogulabs.com>
2023-10-27 12:25:21 -0600

    refactor function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -120,11 +120,6 @@ const Home: NextPage = () => {
               </h2>
 
               <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                {openAIResponse.length === 0 ? (
-                  <div>
-                    <h4>We&apos;re sorry there was an issue</h4>
-                  </div>
-                ) : null}
                 {openAIResponse.length &&
                   openAIResponse?.map((product: any, index) => (
                     <a key={index} href={product.link} className="group">

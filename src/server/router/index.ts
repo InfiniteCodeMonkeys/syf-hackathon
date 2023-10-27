2e2aafa639f1aec3201a2edb11e9a3af7261b458
Mike Dyer <mike@chindogulabs.com>
2023-10-27 11:17:47 -0600

    use api route

--- src/server/router/index.ts
+++
@@ -1,9 +0,0 @@
-import { router } from "../trpcConfig";
-import { openAIRouter } from "./openai";
-
-export const appRouter = router({
-  openAI: openAIRouter,
-});
-
-// export type definition of API
-export type AppRouter = typeof appRouter;
--- src/server/router/openai.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import { publicProcedure, router } from "../trpcConfig";
-import { Configuration, OpenAIApi } from "openai";
-
-export const openAIRouter = router({
-  getResults: publicProcedure
-    .input(z.object({ thing: z.string(), show: z.string() }))
-    .mutation(async ({ input }) => {}),
-});
